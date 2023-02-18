27b47ae5a9e55e5df763187ed8383390eb868f8c
sheeiavellie <98523826+sheeiavellie@users.noreply.github.com>
2023-02-18 16:38:20 +0300

    Implemented command base class

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,16 @@
-﻿namespace MatStatApp.ViewModels
+﻿using MatStatApp.ViewModels.Base;
+
+namespace MatStatApp.ViewModels
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        private string _Title = "MatStat_#1";
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+
     }
 }
